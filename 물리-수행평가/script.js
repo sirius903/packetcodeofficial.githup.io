46db588479947478860287b0402f98bb87af1c1a
sirius903 <sirius4037@gmail.com>
2023-11-01 08:55:58 +0900

    []

--- 물리-수행평가/script.js
+++ 물리-수행평가/script.js
@@ -1,3 +1,3 @@
-for(let i = 3 - document.querySelectorAll('.content').length % 3; i > 0; i--){
+for(let i = 12 - document.querySelectorAll('.content').length % 12; i > 0; i--){
     document.getElementById('contents').innerHTML += `<div class="content none"><div class="content-title">개발중</div></div>`;
 }
