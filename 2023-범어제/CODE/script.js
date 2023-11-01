f9f0e3926903224f90baac97cda23678c7ea19ed
sirius903 <sirius4037@gmail.com>
2023-11-01 12:55:37 +0900

    []

--- 2023-범어제/CODE/script.js
+++ 2023-범어제/CODE/script.js
@@ -2,32 +2,38 @@ const tile = [
     {
         name : 'blank',
         text : '□',
+        movable : true,
         event(){
             //
         }
     },{
         name : 'wall',
         text : '■',
+        movable : false,
         event(){
             movable = false;
         }
     },{
         name : 'able-goal',
         text : '■',
-        event(x, y){
-            if(map[y][x].object == 1){
+        movable : true,
+        event(x, y, dx, dy){
+            if(map[y + dy][x + dx].object == 1){
                 goal = true;
+                chapter += 1;
             }
         }
     },{
         name : 'disable-goal',
         text : '■',
+        movable : true,
         event(){
             //
         }
     },{
         name : 'trap',
         text : '◬',
+        movable : true,
         event(x, y){
             if(map[y][x].object == 1){
                 move(map, x, y, spawnpoint[0] - x, spawnpoint[1] - y);
@@ -39,6 +45,7 @@ const tile = [
     },{
         name : 'button',
         text : '▣',
+        movable : true,
         event(x, y){
             if(map[y][x].object == 1){
                 move(map, x, y, spawnpoint[0] - x, spawnpoint[1] - y);
@@ -50,18 +57,18 @@ const tile = [
     },{
         name : 'blue-potal',
         text : '◉',
-        event(x, y){
+        movable : true,
+        event(x, y, dx, dy){
             let orange = find(map, 0, 7);
-            move(map, x, y, orange[0][0] - x, orange[0][1] - y, true);
-            movable = false;
+            move(map, x + dx, y + dy, orange[0][0] - x - dx, orange[0][1] - y - dy, true);
         }
     },{
         name : 'orange-potal',
         text : '◉',
-        event(x, y){
+        movable : true,
+        event(x, y, dx, dy){
             let blue = find(map, 0, 6);
-            move(map, x, y, blue[0][0] - x, blue[0][1] - y, true);
-            movable = false;
+            move(map, x + dx, y + dy, blue[0][0] - x - dx, blue[0][1] - y - dy, true);
         }
     }
 ];
@@ -84,8 +91,10 @@ const object = [
         name : 'key',
         text : '⨡',
         event(x, y, dx, dy){
-            remove(map, x + dx, y + dy);
-            //
+            find(map, 0, 3).forEach(a => {
+                map[y + dy][x + dx].object = 0;
+                fill(map, a[0], a[1], a[0], a[1], 2);
+            })
         }
     },{
         name : 'box',
@@ -95,6 +104,8 @@ const object = [
         }
     }
 ];
+
+let chapter = 0;
 
 function new_map(width, height){
     let map = [];
@@ -135,23 +146,77 @@ function find(map, type, n){
 }
 
 let movable = true;
-function move(map, x, y, dx, dy, able){
-    if(!able){
-        tile[map[y + dy][x + dx].tile].event(x, y, dx, dy);
-    }
-    if(movable){
-        if(map[y + dy][x + dx].object >= 2){
-            object[map[y + dy][x + dx].object].event(x, y, dx, dy);
+function move(map, x, y, dx, dy, disable){
+    if(y + dy >= 0 && y + dy < map.length && x + dx >= 0 && x + dx < map[0].length){
+        if(tile[map[y + dy][x + dx].tile].movable){
+            if(map[y + dy][x + dx].object >= 2){
+                object[map[y + dy][x + dx].object].event(x, y, dx, dy);
+            }
+            if(map[y + dy][x + dx].object == 0){
+                map[y + dy][x + dx].object = map[y][x].object;
+                map[y][x].object = 0;
+            }
+            if(!disable){
+                tile[map[y + dy][x + dx].tile].event(x, y, dx, dy);
+            }
         }
-        if(map[y + dy][x + dx].object == 0){
-            map[y + dy][x + dx].object = map[y][x].object;
-            map[y][x].object = 0;
-        }
-    }else{
-        movable = true;
     }
     if(goal){
-        //Goal!!!
+        goal = false;
+        document.getElementById('chapter').innerText = chapter;
+        switch (chapter){
+            case 1:
+                map = new_map(14, 14);
+
+                fill(map, 0, 0, 13, 13, 1);
+                fill(map, 1, 1, 12, 12, 0);
+                fill(map, 12, 12, 13, 12, 0);
+                
+                fill(map, 2, 1, 2, 5, 1);
+                fill(map, 1, 7, 4, 7, 1);
+                fill(map, 2, 5, 4, 5, 1);
+                fill(map, 4, 1, 4, 4, 1);
+                fill(map, 6, 2, 6, 5, 1);
+                fill(map, 6, 2, 12, 2, 1);
+                fill(map, 8, 4, 8, 4, 1);
+                fill(map, 7, 5, 8, 5, 1);
+                fill(map, 10, 4, 12, 4, 1);
+                fill(map, 10, 5, 10, 7, 1);
+                fill(map, 6, 7, 12, 7, 1);
+                fill(map, 2, 9, 4, 9, 1);
+                fill(map, 6, 9, 7, 9, 1);
+                fill(map, 9, 9, 12, 9, 1);
+                fill(map, 2, 11, 4, 11, 1);
+                fill(map, 6, 11, 12, 11, 1);
+                fill(map, 4, 8, 4, 8, 1);
+                fill(map, 6, 8, 7, 8, 1);
+                fill(map, 11, 12, 11, 12, 1);
+                fill(map, 2, 10, 2, 10, 1);
+                fill(map, 13, 12, 13, 12, 3); //출구위치
+                
+                fill(map, 7, 4, 7, 4, 6);  //포탈입구
+                fill(map, 12, 12, 12, 12, 7);  //포탈출구
+                
+                spawn(map, 1, 1, 1); //캐릭터 위치
+                
+                // spawn(map, 3, 8, 2); //코인 위치
+                // spawn(map, 11, 5, 2);
+                // spawn(map, 12, 5, 2);
+                // spawn(map, 11, 6, 2);
+                // spawn(map, 12, 6, 2);
+                // spawn(map, 12, 8, 2);
+                // spawn(map, 12, 10, 2);
+                // spawn(map, 12, 3, 2);
+                // spawn(map, 12, 1, 2);
+                // spawn(map, 3, 1, 2);
+                // spawn(map, 3, 2, 2);
+                // spawn(map, 3, 3, 2);
+                // spawn(map, 3, 4, 2);
+                // spawn(map, 3, 10, 2);
+                
+                // spawn(map, 3, 8, 3); //열쇠 위치
+                break;
+        }
     }
     apply(map);
 }
@@ -188,13 +253,11 @@ fill(map, 0, 0, 24, 24, 1);
 fill(map, 1, 1, 23, 23, 0);
 
 fill(map, 10, 10, 10, 10, 2)
-fill(map, 11, 11, 11, 11, 3)
-fill(map, 5, 10, 5, 10, 4)
-fill(map, 10, 5, 10, 5, 5)
-fill(map, 7, 3, 7, 3, 6)
-fill(map, 3, 7, 3, 7, 7)
-
-fill(map, 0, 0, 1, 1, 0)
+// fill(map, 11, 11, 11, 11, 3)
+// fill(map, 5, 10, 5, 10, 4)
+// fill(map, 10, 5, 10, 5, 5)
+// fill(map, 7, 3, 7, 3, 6)
+// fill(map, 3, 7, 3, 7, 7)
 
 spawn(map, 1, 1, 1);
 
--- BlackCyan/main.js
+++ BlackCyan/main.js
@@ -1,11 +1,12 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, updateProfile, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
-import { getFirestore, collection, getDocs, addDoc, setDoc, doc, onSnapshot, initializeFirestore, memoryLocalCache } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js';
+import { getFirestore, collection, getDocs, addDoc, setDoc, doc, deleteDoc , onSnapshot, initializeFirestore, memoryLocalCache } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js';
 import { getDatabase, ref, child, set, get, onValue, onDisconnect, serverTimestamp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDdDEDKiB3BhkHulfzvA9KfFhAWHXRk89w",
   authDomain: "blackcyan-bffc2.firebaseapp.com",
+  databaseURL: "https://blackcyan-bffc2-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "blackcyan-bffc2",
   storageBucket: "blackcyan-bffc2.appspot.com",
   messagingSenderId: "352934571969",
@@ -16,3 +17,172 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const database = getDatabase(app);
+
+const login = function(email, password){
+  signInWithEmailAndPassword(auth, email, password).then((user) => {
+    const statusRef = ref(database, 'user/' + user.user.uid + '/status');
+    set(statusRef, 'online');
+    onValue(ref(database, '.info/connected'), (snapshot) => {
+      if (snapshot.val() === true){
+        onDisconnect(statusRef).set('offline');
+        set(statusRef, 'online');
+        onDisconnect(ref(database, 'user/' + user.user.uid + '/lastOnine')).set(serverTimestamp());
+      }
+    })
+    connect();
+  }).catch((error) => {
+    console.log(error.code);
+    console.log(error.message);
+  });
+}
+
+function randomStr(){
+  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
+  const rnum = Math.floor(Math.random() * chars.length);
+  const randomstring = chars.substring(rnum, rnum + 1);
+  return randomstring
+}
+
+const guest = function(){
+  signInAnonymously(auth).then((user) => {
+    let id = 'Guest-000000';
+    getIds().then(ids => {
+      while(true){
+        id = 'Guest-';
+        for(let i = 0; i < 6; i++){
+          id += randomStr();
+        }
+        if(!ids.includes(id)) break;
+      }
+      updateProfile(auth.currentUser, {
+        displayName : id
+      }).then(() => {
+        setDoc(doc(db, "Auths", user.user.uid), {
+          id : id
+        })
+        set(ref(database, 'user/' + user.user.uid), {
+          displayName : id,
+          email : 'guest',
+          status : 'online',
+          level : 'guest'
+        });
+        onValue(ref(database, '.info/connected'), (snapshot) => {
+          if (snapshot.val() === true){
+            onDisconnect(statusRef).set('offline');
+            set(statusRef, 'online');
+            onDisconnect(ref(database, 'user/' + user.user.uid + '/lastOnine')).set(serverTimestamp());
+          }
+        });
+        connect();
+      }).catch((error) =>{
+        console.log(error.code);
+        console.log(error.message);
+      })
+    })
+  }).catch((error) => {
+    console.log(error.message);
+  })
+}
+
+const signup = function(email, password, id){
+  createUserWithEmailAndPassword(auth, email, password).then((user) => {
+    updateProfile(auth.currentUser, {
+      displayName : id
+    }).then(() => {
+      setDoc(doc(db, "Auths", user.user.uid), {
+        id : id
+      })
+      set(ref(database, 'user/' + user.user.uid), {
+        displayName : id,
+        email : email,
+        status : 'online',
+        level : 1
+      });
+      onDisconnect(ref(database, 'user/' + user.user.uid + '/status')).set('offline');
+      connect();
+    }).catch((error) =>{
+      console.log(error.code);
+      console.log(error.message);
+    })
+  }).catch((error) => {
+    console.log(error.code);
+    console.log(error.message);
+    document.getElementById('signup-error').innerText = error.code;
+    document.getElementById('signup-error').className = 'entry-error error';
+  })
+}
+
+async function getIds(){
+  let id = [];
+  const ids = await getDocs(collection(db, "Auths"));
+  ids.forEach((doc)=>{
+    id.push(doc.data().id);
+  });
+  return id;
+}
+
+function connect(){
+  const user = getAuth().currentUser;
+  document.getElementById('my-id').replaceChildren(user.displayName);
+  onValue(ref(database, 'user/' + user.uid + '/level'), (level) => {
+    document.getElementById('my-level').replaceChildren(level.val() == 'guest' ? 'Guest' : 'Lv. ' + level.val());
+  })
+  onValue(ref(database, 'user'), (snapshot) => {
+      const data = snapshot.val();
+      const players = Object.keys(data).map(i => data[i]).filter(x => x.status != 'offline');
+      document.getElementById('players-list').replaceChildren('');
+      players.forEach(a => {
+          let player = document.createElement('div');
+          player.className = 'player';//------레벨 파이어스토어 적용-------
+          player.innerHTML = `<div class="player-level">${a.level == 'guest' ? 'Guest' : 'Lv. ' + a.level}</div>
+          <div class="player-id">${a.displayName}</div>
+          <div class="player-status ${a.status}" title="${a.status}">●</div>`;
+          document.getElementById('players-list').append(player);
+      })
+      document.getElementById('players-number').innerText = `(${players.length} / 100)`;
+  })
+  document.addEventListener("visibilitychange", () => {
+      if(document.hidden){
+          set(ref(database, 'user/' + user.uid + '/status'), 'away');
+      }else{
+          set(ref(database, 'user/' + user.uid + '/status'), 'online');
+      }
+  });
+  document.querySelector('.main-menu').className = 'main-menu container hidden';
+  document.querySelector('.list').className = 'list container';
+  // onValue(ref(database, 'rooms'), (snapshot) => {
+  //   let rooms = Object.keys(snapshot.val()).map(i => snapshot.val()[i]);
+  //   document.getElementById('rooms').innerHTML = '';
+  //   var add_item = document.createElement('div');
+  //   add_item.className = 'rooms_item add_btn';
+  //   add_item.innerHTML = '<div id="text">New Room</div>';
+  //   add_item.addEventListener("click", ()=>{
+  //     enter(false);
+  //   })
+  //   document.getElementById('rooms').append(add_item);
+  //   rooms.forEach(a => {
+  //     let item = document.createElement('div');
+  //     item.className = 'rooms_item';
+  //     item.innerHTML = `<div class="rooms-title">${a.name}</div><div class="rooms-limit">${'1 / ' + a.limit}</div>`;
+  //     item.addEventListener("click", () => {enter(a.id);});
+  //     document.getElementById('rooms').append(item);
+  //   })
+  //   for(let i = 3 - rooms.length % 3; i > 1; i--){
+  //     let item = document.createElement('div');
+  //     item.className = 'rooms_item';
+  //     document.getElementById('rooms').append(item);
+  //   }
+  // });
+
+  document.getElementById('full-chat-input').addEventListener("keydown", function(e){
+    if(e.code === 'Enter'){
+      setDoc(doc(db, "Chats", "Full-Chats"), {
+        tag : auth.currentUser.displayName,
+        message : document.getElementById('full-chat-input').value
+      })
+      document.getElementById('full-chat-input').value = '';
+    }
+  })
+}
+
+export { login, signup, getIds, guest };
