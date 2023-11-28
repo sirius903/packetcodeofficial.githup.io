85bb006c550b29672564f01bc00648106c541822
sirius903 <sirius4037@gmail.com>
2023-11-28 16:20:18 +0900

    []

--- BlackCyan/main.js
+++ BlackCyan/main.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, updateProfile, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
 import { getFirestore, collection, getDocs, addDoc, setDoc, doc, deleteDoc , onSnapshot, initializeFirestore, memoryLocalCache } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js';
-import { getDatabase, ref, child, set, get, onValue, onDisconnect, serverTimestamp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js';
+import { getDatabase, ref, child, set, get, onValue, onDisconnect, serverTimestamp, off } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDdDEDKiB3BhkHulfzvA9KfFhAWHXRk89w",
@@ -17,6 +17,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const database = getDatabase(app);
+let chat_type = ['Main', '', ''];
 
 /**로그인 함수
  * @param {String} email 이메일
@@ -159,7 +160,7 @@ function load(uid){
     }
   });
   document.querySelector('.main-menu').className = 'main-menu container hidden';
-  document.querySelector('.list').className = 'list container';
+  document.querySelector('.list').className = 'list container scroller';
   onValue(ref(database, 'Rooms/Public'), (snapshot) => {
     let rooms = Object.keys(snapshot.val()).map(i => snapshot.val()[i]);
     document.getElementById('rooms').innerHTML = '';
@@ -179,9 +180,23 @@ function load(uid){
     })
   })
 
+  chat_change(0);
+}
+
+const chat_change = function(type, id){
   document.getElementById('full-chat-input').addEventListener("keypress", function(e){
+    console.log(1)
     if(e.code === 'Enter'){
-      set(ref(database, "Chats/Main"), {
+      console.log(2)
+      let chatRef;
+      if(type == 0){
+        chatRef = 'Chats/Main';
+      }else{
+        chatRef = 'Chats/Public/' + sha224(id);
+      }
+      // let chatRef = 'Chats/' + (type == 0 ? 'Main' : ('Public/' + sha224(id)));
+      console.log(chatRef);
+      set(ref(database, chatRef), {
         id : auth.currentUser.displayName,
         message : document.getElementById('full-chat-input').value
       })
@@ -189,32 +204,71 @@ function load(uid){
       document.getElementById('full-chat-input').value = '';
     }
   })
-
-  onValue(ref(database, "Chats/Main"), (snapshot) => {
-    let chat = snapshot.val();
-    if(chat.message != ''){
-      let full_chat = document.getElementById('full-chat');
-      let scroll = true;
-      if(full_chat.scrollTop + full_chat.clientHeight + 5 < full_chat.scrollHeight) scroll = false;
-      let div = document.createElement('div');
-      let h1 = document.createElement('h1');
-      let p = document.createElement('p');
-      h1.innerText = chat.id;
-      p.innerText = chat.message;
-      div.className = 'chat-message';
-      h1.className = 'chat-tag';
-      div.append(h1);
-      div.append(p);
-      full_chat.append(div);
-      if(scroll) full_chat.scrollTop = full_chat.scrollHeight;
-      set(ref(database, "Chats/Main/message"), '');
-    }
-  })
+  // console.log(1)
+  // function chat_enter(e){
+  //   if(e.code === 'Enter'){
+  //   }
+  // }
+  // document.getElementById('full-chat-input').removeEventListener("keypress", chat_enter);
+  // document.getElementById('full-chat-input').addEventListener("keypress", chat_enter);
+  //메인 + 룸 (방 참가시)1
+  //메인만 (기본)0
+  //룸(설정 시)2
+  off(ref(database, 'Chats/Main'));
+  // if(id) off(ref(database));
+  switch (type) {
+    case 1:
+      onValue(ref(database, 'Chats/Public/' + sha224(id)), (snapshot) => {
+        let chat = snapshot.val();
+        if(chat.message != ''){
+          let full_chat = document.getElementById('full-chat');
+          let scroll = true;
+          if(full_chat.scrollTop + full_chat.clientHeight + 5 < full_chat.scrollHeight) scroll = false;
+          let div = document.createElement('div');
+          let h1 = document.createElement('h1');
+          let p = document.createElement('p');
+          h1.innerText = 'In-Room' + chat.id;
+          p.innerText = chat.message;
+          div.className = 'chat-message';
+          h1.className = 'chat-tag';
+          div.append(h1);
+          div.append(p);
+          full_chat.append(div);
+          if(scroll) full_chat.scrollTop = full_chat.scrollHeight;
+          set(ref(database, 'Chats/Public /message'), '');
+        }
+      })
+    case 0:
+      onValue(ref(database, 'Chats/Main'), (snapshot) => {
+        let chat = snapshot.val();
+        if(chat.message != ''){
+          let full_chat = document.getElementById('full-chat');
+          let scroll = true;
+          if(full_chat.scrollTop + full_chat.clientHeight + 5 < full_chat.scrollHeight) scroll = false;
+          let div = document.createElement('div');
+          let h1 = document.createElement('h1');
+          let p = document.createElement('p');
+          h1.innerText = chat.id;
+          p.innerText = chat.message;
+          div.className = 'chat-message';
+          h1.className = 'chat-tag';
+          div.append(h1);
+          div.append(p);
+          full_chat.append(div);
+          if(scroll) full_chat.scrollTop = full_chat.scrollHeight;
+          set(ref(database, 'Chats/Main/message'), '');
+        };
+      });
+      break;
+    default:
+      //뭐요
+      break;
+  }
 }
 
 const create = function(uid){
-  document.getElementById('room-title').innerText = auth.currentUser.displayName + "'s Room";
-  document.getElementById('room-name').value = auth.currentUser.displayName + "'s Room";
+  document.getElementById('room_title').innerText = auth.currentUser.displayName + "'s Room";
+  document.getElementById('room-title').value = auth.currentUser.displayName + "'s Room";
   document.querySelector('.rooms-list').className = 'rooms-list hidden';
   document.querySelector('.room').className = 'room';
   document.getElementById('create-button').addEventListener("click", async function(){
@@ -229,7 +283,7 @@ const create = function(uid){
       set(ref(database, 'Rooms/Public/' + sha224(id)), {
         id : id,
         limit : parseInt(document.getElementById('room-limit').value),
-        title : document.getElementById('room-name').value,
+        title : document.getElementById('room-title').value,
         manager : false,
         player1 : false,
         player2 : false,
@@ -238,7 +292,7 @@ const create = function(uid){
         players : 0
       })
       enter(uid, id);
-      onDisconnect(ref(database, 'Rooms/Public/' + sha224(id))).remove();
+      // onDisconnect(ref(database, 'Rooms/Public/' + sha224(id))).remove();
     })
   })
 }
@@ -246,11 +300,14 @@ const create = function(uid){
 const enter = function(uid, id){
   get(child(ref(database), 'Rooms/Public/' + sha224(id))).then((snapshot)=>{
     const data = snapshot.val();
+    function roomRef(text){return ref(database, 'Rooms/Public/' + sha224(id) + text);};
     if(data.limit > data.players){
+      let number;
       function change(n){
-        set(ref(database, 'Rooms/Public/' + sha224(id) + '/player' + n), uid);
+        set(roomRef('/player' + n), uid);
+        number = n;
         if(!data.manager){
-          set(ref(database, 'Rooms/Public/' + sha224(id) + '/manager'), uid);
+          set(roomRef('/manager'), uid);
         }
       }
       if(!data.player1){
@@ -262,14 +319,107 @@ const enter = function(uid, id){
       }else if(!data.player4){
         change(4);
       }
-      set(ref(database, 'Rooms/Public/' + sha224(id) + '/players'), data.players + 1);
-      console.log(data);
-      alert(uid+ ' ' + id)
-      document.getElementById('room-title').innerText = data.title;
-      document.getElementById('room-name').value = data.title;
+      set(ref(database, 'Users/' + uid + '/room'), sha224(id));
+      set(roomRef('/players'), data.players + 1);
+      // console.log(data);
+      // alert(id + ', ' + uid);
+      document.getElementById('room-players-title').className = '';
+      document.getElementById('room_title').innerText = data.title;
+      document.getElementById('room-title').value = data.title;
+      document.getElementById('create-button').className = 'room-btn hidden';
       document.querySelector('.rooms-list').className = 'rooms-list hidden';
       document.querySelector('.room').className = 'room';
-      // onDisconnect(ref(database, 'Rooms/Public/' + sha224(id))).remove();
+      onDisconnect(ref(database, 'Users/' + uid + '/room')).set('');
+      // chat_change(1, id);
+      document.getElementById('room-chat-box').className = 'settings';
+      document.getElementById('room-chat-input').addEventListener("keypress", function(e){
+        if(e.code === 'Enter'){
+          set(ref(database, 'Chats/Public/' + sha224(id)), {
+            id : auth.currentUser.displayName,
+            message : document.getElementById('room-chat-input').value
+          })
+          //firestore에 메시지 저장
+          document.getElementById('room-chat-input').value = '';
+        }
+      })
+      onValue(ref(database, 'Chats/Public/' + sha224(id)), (snapshot) => {
+        let chat = snapshot.val();
+        if(chat.message != ''){
+          let room_chat = document.getElementById('room-chat');
+          let scroll = true;
+          if(room_chat.scrollTop + room_chat.clientHeight + 5 < room_chat.scrollHeight) scroll = false;
+          let div = document.createElement('div');
+          let h1 = document.createElement('h1');
+          let p = document.createElement('p');
+          h1.innerText = chat.id;
+          p.innerText = chat.message;
+          div.className = 'chat-message';
+          h1.className = 'chat-tag';
+          div.append(h1);
+          div.append(p);
+          room_chat.append(div);
+          if(scroll) room_chat.scrollTop = room_chat.scrollHeight;
+          set(ref(database, 'Chats/Public/' + sha224(id)) + '/message', '');
+        }
+      })
+      onValue(roomRef(''), (snapshot) => {
+        const room = snapshot.val();
+        onDisconnect(roomRef('')).cancel();
+        document.getElementById('room-players').replaceChildren('');
+        [room.player1, room.player2, room.player3, room.player4].forEach(a => {
+          if(a){
+            get(child(ref(database), 'Users/' + a)).then((b) => {
+              let user = b.val()
+              let player = document.createElement('div');
+              player.className = 'room-player';
+              player.innerHTML = `<img class="room-player-avatar" src="https://fastly.picsum.photos/id/251/250/250.jpg?hmac=_NUJeiKDfHVqhYGCW_85ojnjvg9ZWfHfMGppohybKyE">
+              <div class="room-player-text">
+                <div class="room-player-id">${user.id}</div>
+                <div class="room-player-data">
+                  <div class="room-player-level">Lv. ${user.exp}</div>
+                  ${a == room.manager ? '<div class="room-player-host">Host</div>' : ''}
+                </div>
+              </div>`;
+              document.getElementById('room-players').append(player);
+            })
+          }
+        })
+        document.getElementById('room-players-number').innerText = `(${room.players} / ${room.limit})`;
+        document.getElementById('room_title').innerText = room.title;
+        if(uid == room.manager){
+          document.getElementById('room-configs').className = 'after';
+          document.getElementById('change-button').className = 'room-btn';
+          document.getElementById('start-button').className = 'room-btn' + (room.players > 1 ? ' active' : '');
+          document.querySelectorAll('.config_item').forEach(a => {
+            a.addEventListener("change", function(){
+              document.getElementById('change-button').className = 'room-btn active';
+            })
+          })
+          document.getElementById('change-button').addEventListener("click", function(){
+            ['title', 'limit'].forEach(a => {
+              set(roomRef('/' + a), document.getElementById('room-' + a).value);
+            })
+          })
+        }
+        onDisconnect(roomRef('/players')).set(room.players - 1);
+        onDisconnect(roomRef('/player' + number)).set(false);
+        if(uid == room.manager){
+          if(room.players > 1){
+            if(room.player1 && room.player1 != room.manager){
+              onDisconnect(roomRef('/manager')).set(room.player1);
+            }else if(room.player2 && room.player2 != room.manager){
+              onDisconnect(roomRef('/manager')).set(room.player2);
+            }else if(room.player3 && room.player3 != room.manager){
+              onDisconnect(roomRef('/manager')).set(room.player3);
+            }else if(room.player4 && room.player4 != room.manager){
+              onDisconnect(roomRef('/manager')).set(room.player4);
+            }
+          }else{
+            onDisconnect(roomRef('')).remove();
+          }
+        }
+        // alert(room.manager + ', ' + room.players);
+      });
     }else{
       alert('방 꽉 참');
     }
