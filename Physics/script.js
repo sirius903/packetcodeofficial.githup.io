7e3d03b5e281a7c4a9b7d5d5f1391fe7e1b22737
sirius903 <sirius4037@gmail.com>
2024-03-08 13:20:11 +0900

    []

--- Physics/script.js
+++ Physics/script.js
@@ -4,7 +4,7 @@ document.querySelectorAll('.path').forEach(path => {
 })
 
 let delta = 0;
-let years = ['Physics', 'B.C 600', '1600', '1666', '1729', '1746', '1752', '1780', '1785', '1800', '1827', '1831', '1840', '1864', '1879', '1882', '1888', '1895', '1897'];
+let years = ['Physics', 'B.C 600', '1600', '1666', '1687', '1729', '1746', '1752', '1780', '1785', '1800', '1827', '1831', '1840', '1864', '1879', '1882', '1888', '1895', '1897'];
 window.addEventListener("wheel", function(e){
     if(e.deltaY > 0){
         if(delta + e.deltaY > 0){
@@ -15,6 +15,16 @@ window.addEventListener("wheel", function(e){
     }else if(e.deltaY < 0){
         delta += e.deltaY;
     }
+    moveWindow();
+    localStorage.setItem("delta", delta);
+});
+window.onload = function(){
+    localStorage.setItem("delta", localStorage.getItem("delta") ?? 0);
+    delta = +localStorage.getItem("delta");
+    moveWindow();
+}
+
+function moveWindow(){
     contents.style.transform = `translate(${delta}px)`;
     year.innerText = years[Math.floor(-delta / 700)];
     document.querySelectorAll('.path').forEach((path, i) => {
@@ -31,4 +41,4 @@ window.addEventListener("wheel", function(e){
             path.style.transition = '';
         }
     })
-});
+}
