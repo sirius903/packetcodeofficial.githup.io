2ae97cae3bb922f79917c9ef23e37fb2b3151fb3
sirius903 <sirius4037@gmail.com>
2024-03-12 16:25:01 +0900

    []

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
+const password = '95582e1021cd7ebc39cc552519680686084b3bfea5dbfa15c05503150004d9cd';
+
 function operater(x){
-    console.log(x);
-    alert("옳바르지 않은 비밀번호입니다.");
-}
-
-let banners = new Array(document.querySelectorAll('.banner').length).fill().map((x, i) => i);
-
-function banner(x){
-    document.querySelectorAll('.banner').forEach(a => {
-        a.style.transition = '';
-    })
-    if(x == 1){
-        banners.unshift(banners.pop());
-        document.querySelectorAll('.banner')[banners[0]].style.transition = '0s all';
-    }else if(x == -1){
-        document.querySelectorAll('.banner')[banners[0]].style.transition = '0s all';
-        banners.push(banners.shift());
-    }
-    banners.forEach((a, i) => {
-        document.querySelectorAll('.banner')[a].style.translate = `${100 * (i - a - 1)}% 0`;
-    })
-}
-banner(1);
-
-function bannerTo(){
-    if(banners[1] == 0){
-        window.location.href = "#app";
+    if(sha256(x) == password){
+        alert("관리자 전환 완료.");
+    }else{
+        alert("옳바르지 않은 비밀번호입니다.");
     }
 }
+
+// let banners = new Array(document.querySelectorAll('.banner').length).fill().map((x, i) => i);
+
+// function banner(x){
+//     document.querySelectorAll('.banner').forEach(a => {
+//         a.style.transition = '';
+//     })
+//     if(x == 1){
+//         banners.unshift(banners.pop());
+//         document.querySelectorAll('.banner')[banners[0]].style.transition = '0s all';
+//     }else if(x == -1){
+//         document.querySelectorAll('.banner')[banners[0]].style.transition = '0s all';
+//         banners.push(banners.shift());
+//     }
+//     banners.forEach((a, i) => {
+//         document.querySelectorAll('.banner')[a].style.translate = `${100 * (i - a - 1)}% 0`;
+//     })
+// }
+// banner(1);
+
+// function bannerTo(){
+//     if(banners[1] == 0){
+//         window.location.href = "#app";
+//     }
+// }
